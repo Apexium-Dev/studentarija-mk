d5adedf8941f280bd34f23fb68741ddb66f5518c
Mahmut Miftaroski <miftarosky@hotmail.com>
2026-06-20 16:38:45 +0200

    fix: set html lang to mk to match default LanguageProvider locale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="mk"
       className={`${geistSans.variable} ${geistMono.variable} ${oswald.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
